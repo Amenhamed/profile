0892ef43fc46f39c795ab921232f80fd52e88df5
amen <amenhamedd@gmail.com>
2025-09-10 23:55:58 +0100

    F9

--- project/settings.py
+++ project/settings.py
@@ -114,10 +114,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS =[os.path.join(BASE_DIR , 'project/static')]
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR , 'project/static')]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project/static'),  # لو عندك فولدر static داخل app اسمه project
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
