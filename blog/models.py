4ed4a96090caf79bfe90cbbe44b5bdde9b31d477
amen <amenhamedd@gmail.com>
2025-09-14 03:10:05 +0100

    fixed comment 2

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 class comment (models.Model):
     name = models.CharField(max_length=50)  # اسم الزائر
-    comment = models.CharField()
+    comment = models.CharField(max_length=500)  # نص التعليق (حد أقصى 500 حرف مثلاً)
     session_id = models.CharField(max_length=40)  # معرّف الزائر
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
